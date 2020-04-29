32c4efcbe369a590320851c8a8f5e0af49040a6e
lukassplitthoff <lukas.splitthoff@gmail.com>
2020-04-29 13:04:35 +0200

    correct label

--- solidstate/theorySoSt.py
+++ solidstate/theorySoSt.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.constants as pyc
 
-class JosephsonJunction(object):
+class solidstate(object):
     @staticmethod
     def energy_zeeman(gl, L, gs, S, B, E_offset):
         return E_offset + pyc.e / 2 / pyc.m_e * (gl * L + gs * S) * B
@@ -13,8 +13,8 @@ class JosephsonJunction(object):
 
 def _example():
 
-    e1 = JosephsonJunction.energy_zeeman(gl=1, L=0, gs=2, S=1/2, B=np.linspace(0,1, 1001), E_offset=0)
-    e2 = JosephsonJunction.energy_zeeman_simple(ms=1, ge=2,  B=np.linspace(0, 1, 1001), E_offset=0)
+    e1 = solidstate.energy_zeeman(gl=1, L=0, gs=2, S=1/2, B=np.linspace(0,1, 1001), E_offset=0)
+    e2 = solidstate.energy_zeeman_simple(ms=1, ge=2,  B=np.linspace(0, 1, 1001), E_offset=0)
     print('Zeeman energy', e1)
     print('Zeeman energy', e2)
 
