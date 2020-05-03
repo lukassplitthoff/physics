358872e5fafd1b3c52a751ef826784a10dcc0d8b
LJSplitthoff <lukas.splitthoff@gmail.com>
2020-05-03 12:17:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Physics',
-      version='0.0.1',
-      description='Fun with physics',
-      author='Lukas Splitthoff',
-      author_email='lukas.splitthoff@gmail.com',
-      url='https://github.com/lukassplitthoff/physics',
-      packages=['physics'])
+setup(
+    name='Physics',
+    version='0.0.1',
+    description='Fun with physics',
+    author='Lukas Splitthoff',
+    author_email='lukas.splitthoff@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/lukassplitthoff',
+)
